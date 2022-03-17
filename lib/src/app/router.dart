8079c9a1bb078154b5d8efeb6dfd82691850795c
ArnoldFrb <arnoldfrb@gmail.com>
2022-03-17 15:25:25 -0500

    Aplicación de  Clean Architecture y creación de archivo router.dart

---
+++ lib/src/app/router.dart
@@ -0,0 +1,21 @@
+import 'package:cine_house/src/app/pages/home/home_view.dart';
+import 'package:flutter/material.dart';
+
+const String homeRoute = '/';
+const String signInRoute = '/sign-in';
+
+class RouterDynamic {
+  static Route<dynamic> generateRoute(RouteSettings settings){
+    switch(settings.name){
+      case homeRoute: return MaterialPageRoute(builder: (_) => const HomePage(title: 'Hello world'));
+      case signInRoute: return MaterialPageRoute(builder: (_) => const HomePage(title: 'Sign In'));
+      default: return MaterialPageRoute(builder: (_) => const Scaffold(
+        body: Center(
+          child: Text('Hola'),
+        ),
+      )
+    );
+  }
+}
+}
+
