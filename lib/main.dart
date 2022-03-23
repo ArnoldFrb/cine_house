a1897e6868dd48c135345c303ac1c348bdc00f76
ArnoldFrb <arnoldfrb@gmail.com>
2022-03-23 13:24:02 -0500

    Configuración de orientación vertical de la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cine_house/src/app/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:cine_house/src/di/injection_container.dart' as di;
 
 void main() async {
@@ -13,6 +14,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
     return MaterialApp(
       title: 'Cine House',
       onGenerateRoute: RouterDynamic.generateRoute,
